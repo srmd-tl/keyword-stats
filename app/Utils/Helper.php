d505e1a81dfa2c4c1c660f15db1ef474974665c4
Sarmad Sohail <sarmad-sohail@hotmail.com>
2021-12-12 15:02:43 +0500

    Fetch kw stats using kewywordeverywhere api

--- app/Utils/Helper.php
+++ app/Utils/Helper.php
@@ -5,7 +5,6 @@ namespace App\Utils;
 
 
 use Illuminate\Support\Facades\Http;
-use Symfony\Component\DomCrawler\Crawler;
 
 class Helper
 {
@@ -16,8 +15,26 @@ class Helper
      */
     public static function fetchGoogleSearch($keyword)
     {
-        $client = new \GoogleSearch("ea1008b2766e4f46760b50c2850b95f6b5dc5cb4499724d5b13515cbfec3e7ea");
-        $query = ["q" => 'Intitle:"'.$keyword.'"'];
-        return  $client->get_json($query);
+        $client = new \GoogleSearch(env('SERP_API'));
+        $query = ["q" => 'Intitle:"' . $keyword . '"'];
+        return $client->get_json($query);
     }
+
+    /**
+     * @param $keyword
+     * @return array|mixed
+     */
+    public static function fetchStats($keyword)
+    {
+        return Http::asForm()->withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => sprintf('%s %s', 'Bearer', env('KWEW_API'))
+        ])
+            ->post('https://api.keywordseverywhere.com/v1/get_keyword_data', [
+                'kw[0]' => $keyword,
+            ])
+            ->json();
+    }
+
 }
+
