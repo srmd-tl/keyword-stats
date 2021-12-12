66c471041301d5a93de4c43ae8028b21b4f85364
Sarmad Sohail <sarmad-sohail@hotmail.com>
2021-12-12 16:28:33 +0500

    updates

--- app/Utils/Helper.php
+++ app/Utils/Helper.php
@@ -5,6 +5,7 @@ namespace App\Utils;
 
 
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 class Helper
 {
@@ -20,6 +21,33 @@ class Helper
         return $client->get_json($query);
     }
 
+    /**\
+     * @param $keyword
+     * @return array
+     */
+    public static function purifyData($keyword)
+    {
+        $inTitle = [];
+        $inUrl = [];
+        $inDescription = [];
+
+        $data = self::fetchGoogleSearch($keyword);
+        $totalResults = $data->search_information->total_results;
+        $results = $data->organic_results;
+        foreach ($results as $result) {
+            if ($result->title && Str::contains(strtolower($result->title), strtolower($keyword))) {
+                $inTitle[] = $result->title;
+            }
+            if ($result->link && Str::contains(strtolower($result->link), strtolower(str_replace(' ','-',$keyword)))) {
+                $inUrl[] = $result->link;
+            }
+            if ($result->snippet && Str::contains(strtolower($result->snippet),strtolower($keyword))) {
+                $inDescription[] = $result->snippet;
+            }
+        }
+        return ['inTitle' => $inTitle, 'inDescription' => $inDescription, 'inUrl' => $inUrl, 'totalResults' => $totalResults];
+    }
+
     /**
      * @param $keyword
      * @return array|mixed
@@ -33,7 +61,7 @@ class Helper
             ->post('https://api.keywordseverywhere.com/v1/get_keyword_data', [
                 'kw[0]' => $keyword,
             ])
-            ->json();
+            ->object();
     }
 
 }
