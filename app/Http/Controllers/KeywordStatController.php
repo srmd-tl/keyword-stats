40efa7400d5df9851e701f54e525a3edaaaf8659
Sarmad Sohail <sarmad-sohail@hotmail.com>
2021-12-12 22:50:03 +0500

    updates

--- app/Http/Controllers/KeywordStatController.php
+++ app/Http/Controllers/KeywordStatController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\KeywordStat;
 use App\Utils\Helper;
 use Illuminate\Http\Request;
 use TCG\Voyager\Http\Controllers\VoyagerBaseController as BaseVoyagerBaseController;
@@ -11,9 +12,10 @@ class KeywordStatController extends BaseVoyagerBaseController
     public function store(Request $request)
     {
         //fetch stats from keywordeverywhere api
-//        $kwStats = Helper::fetchStats($request->keyword);
-        if (false && $kwStats) {
-            $kwStats = current($kwStats);
+        $kwStats = Helper::fetchStats($request->keyword);
+        $difficulty = 0;
+        if ($kwStats) {
+            $kwStats = current($kwStats->data);
             $vol = $kwStats->vol;
             $cpcVal = $kwStats->cpc->value;
             $competition = $kwStats->competition;
@@ -24,13 +26,21 @@ class KeywordStatController extends BaseVoyagerBaseController
         //generate Intitle,Inurl etc
         $googleStats = Helper::purifyData($request->keyword);
         if ($googleStats) {
+            if ($googleStats['totalResults'] <= 5) {
+                $difficulty = 0;
+            } else if ($googleStats['totalResults'] >= 11 && $googleStats['totalResults'] <= 50) {
+                $difficulty = 1;
+            } else if ($googleStats['totalResults'] >= 51 && $googleStats['totalResults'] <= 100) {
+                $difficulty = 2;
+            }
             $request->request->add(['kw_in_url' => count($googleStats['inUrl'])]);
             $request->request->add(['kw_in_title' => count($googleStats['inTitle'])]);
             $request->request->add(['kw_in_description' => count($googleStats['inDescription'])]);
-            $request->request->add(['cpc' => $cpcVal]);
-            $request->request->add(['competition' => $competition]);
+            $request->request->add(['difficulty' => $difficulty]);
+            $request->request->add(['ratio' => $googleStats['totalResults'] / $vol]);
+            $request->request->add(['in_title_url_result' => $googleStats['totalResults']]);
         }
-
-
+        KeywordStat::firstOrCreate(['keyword' => $request->keyword], $request->except('_token'));
+        return redirect()->route('voyager.keyword-stats.index');
     }
 }
--- app/Models/KeywordStat.php
+++ app/Models/KeywordStat.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class KeywordStat extends Model
 {
     use HasFactory;
+    protected $guarded=[];
 }
