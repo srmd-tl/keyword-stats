f64b22a90eefb4de0f698b8a5c505e529e59e521
Sarmad Sohail <sarmad-sohail@hotmail.com>
2021-12-18 12:32:05 +0500

    difficulty accessor

--- app/Models/KeywordStat.php
+++ app/Models/KeywordStat.php
@@ -8,5 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class KeywordStat extends Model
 {
     use HasFactory;
-    protected $guarded=[];
+
+    protected $guarded = [];
+
+    public function getDifficultyAttribute($value)
+    {
+        if ($value <= 10) {
+            $value = "Very Easy";
+        } else if ($value > 10 && $value <= 20) {
+            $value = "Easy";
+        } else if ($value > 20 && $value <= 50) {
+            $value = "Medium";
+        } else if ($value > 50 && $value <= 100) {
+            $value = "Hard";
+        } else if ($value > 100) {
+            $value = "Very Hard";
+        }
+        return $value;
+    }
 }
