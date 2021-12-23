9683fd5ebd7232d409db0333f2252d2492d39737
Sarmad Sohail <sarmad-sohail@hotmail.com>
2021-12-23 22:37:26 +0500

    updates

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $data=\App\Utils\Helper::fetchGoogleSearch('kitchen remodeling');
+//    $data=\App\Utils\Helper::fetchGoogleSearch('kitchen remodeling');
 //    $client = new GoogleSearch("ea1008b2766e4f46760b50c2850b95f6b5dc5cb4499724d5b13515cbfec3e7ea");
 //    $query = ["q" => 'Intitle:"kitchen remodeling"'];
 //    $response = $client->get_json($query);
-    dd($data);
+//    dd($data);
     return redirect()->route('voyager.dashboard');
     $data = "https://www.bhg.com/kitchen/remodeling/planning/kitchen-remodeling/";
     dd(\Illuminate\Support\Str::contains($data,strtolower(str_replace(' ','-','kitchens remodeling'))));
