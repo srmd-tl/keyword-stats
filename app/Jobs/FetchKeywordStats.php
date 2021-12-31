c04b08a92fcfa649724ad40231ca4aadea54c441
Sarmad Sohail <sarmad-sohail@hotmail.com>
2021-12-31 18:14:29 +0500

    exception handling

--- app/Jobs/FetchKeywordStats.php
+++ app/Jobs/FetchKeywordStats.php
@@ -41,48 +41,54 @@ class FetchKeywordStats implements ShouldQueue
         //fetch stats from keywrodio api
         $kwStats = Helper::fetchStatsUsingKeywordIo($this->keyword);
         for ($i = 0; $i < 20; $i++) {
-            $temp = [];
-            if ($kwStats) {
-                $kwStatsObj = $kwStats->keywords[$i];
-                $vol = $kwStatsObj->n;
-                $cpcVal = $kwStatsObj->sb;
-                $competition = $kwStatsObj->c;
-                $temp['search_volume'] = $vol;
-                $temp['cpc'] = $cpcVal;
-                $temp['competition'] = $competition;
-            }
-            //generate Intitle,Inurl etc
-            $googleStats = Helper::purifyData($kwStatsObj->kw);
-            $difficulty = 0;
-            if ($googleStats) {
-                if ($googleStats['totalResults'] >= 0 && $googleStats['totalResults'] <= 10) {
-                    $difficulty = 0;
-                } else if ($googleStats['totalResults'] >= 11 && $googleStats['totalResults'] <= 20) {
-                    $difficulty = 1;
-                } else if ($googleStats['totalResults'] >= 21 && $googleStats['totalResults'] <= 50) {
-                    $difficulty = 2;
-                } else if ($googleStats['totalResults'] >= 51 && $googleStats['totalResults'] <= 100) {
-                    $difficulty = 3;
-                } else if ($googleStats['totalResults'] > 100) {
-                    $difficulty = 4;
+            try {
+
+                $temp = [];
+                if ($kwStats) {
+                    $kwStatsObj = $kwStats->keywords[$i];
+                    $vol = $kwStatsObj->n;
+                    $cpcVal = $kwStatsObj->sb;
+                    $competition = $kwStatsObj->c;
+                    $temp['search_volume'] = $vol;
+                    $temp['cpc'] = $cpcVal;
+                    $temp['competition'] = $competition;
                 }
-                $temp['kw_in_url'] = count($googleStats['inUrl']);
-                $temp['kw_in_title'] = count($googleStats['inTitle']);
-                $temp['kw_in_description'] = count($googleStats['inDescription']);
-                $temp['difficulty'] = $difficulty;
-                $temp['keyword'] = $kwStatsObj->kw;
-                $temp['user_id'] = $this->userId;
-                $temp['project_id'] = $this->projectId;
+                //generate Intitle,Inurl etc
+                $googleStats = Helper::purifyData($kwStatsObj->kw);
+                $difficulty = 0;
+                if ($googleStats) {
+                    if ($googleStats['totalResults'] >= 0 && $googleStats['totalResults'] <= 10) {
+                        $difficulty = 0;
+                    } else if ($googleStats['totalResults'] >= 11 && $googleStats['totalResults'] <= 20) {
+                        $difficulty = 1;
+                    } else if ($googleStats['totalResults'] >= 21 && $googleStats['totalResults'] <= 50) {
+                        $difficulty = 2;
+                    } else if ($googleStats['totalResults'] >= 51 && $googleStats['totalResults'] <= 100) {
+                        $difficulty = 3;
+                    } else if ($googleStats['totalResults'] > 100) {
+                        $difficulty = 4;
+                    }
+                    $temp['kw_in_url'] = count($googleStats['inUrl']);
+                    $temp['kw_in_title'] = count($googleStats['inTitle']);
+                    $temp['kw_in_description'] = count($googleStats['inDescription']);
+                    $temp['difficulty'] = $difficulty;
+                    $temp['keyword'] = $kwStatsObj->kw;
+                    $temp['user_id'] = $this->userId;
+                    $temp['project_id'] = $this->projectId;
 
 
-                if ($vol > 0) {
-                    $temp['ratio'] = $googleStats['totalResults'] / $vol;
+                    if ($vol > 0) {
+                        $temp['ratio'] = $googleStats['totalResults'] / $vol;
+                    }
+                    $temp['in_title_url_result'] = $googleStats['totalResults'];
                 }
-                $temp['in_title_url_result'] = $googleStats['totalResults'];
+                KeywordStat::firstOrCreate(['keyword' => $kwStatsObj->kw], $temp);
+                $user = User::find($this->userId);
+                $user->update(['remaining_keywords' => $user->remaining_keywords - 1]);
+            } catch (\Exception $e) {
+                logger(sprintf('Something wrong %s', $e->getMessage()));
             }
-            KeywordStat::firstOrCreate(['keyword' => $kwStatsObj->kw], $temp);
-            $user = User::find($this->userId);
-            $user->update(['remaining_keywords' => $user->remaining_keywords - 1]);
+
 
         }
     }
